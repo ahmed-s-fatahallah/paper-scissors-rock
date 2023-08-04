0b790ec9bcf31191403d0a9aaf721ad43a25b851
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2023-08-04 17:32:52 +0300

    fix: Query the main section to add the event listeners on it

--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 export const DOM = {
   scoreCounterEl: document.querySelector(".score-container__counter"),
-  gameSectionEl: document.querySelector(".game-section"),
   rulesBtnEl: document.querySelector(".rules-btn"),
   rulesModalEl: document.querySelector(".rules-modal"),
   rulesWindowBtnEl: document.querySelector(".rules-header__btn"),
+  mainSectionEl: document.querySelector(".main-section"),
 };
