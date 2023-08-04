2a0eb083570363773bb5184c47d1b2db4d23bfc7
Ahmed Saeed <ahmed.s.fatahallah@gmail.com>
2023-08-04 17:38:08 +0300

    feat: Add the logic for generating rnd choice

--- model.js
+++ model.js
@@ -1,1 +1,8 @@
-const score = 0;
+let score = 0;
+
+export const generateHousePickIndex = (arr) => {
+  if (!arr) return;
+  const rndIndex = Math.floor(Math.random() * arr.length);
+
+  return rndIndex;
+};
